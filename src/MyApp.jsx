b82c1b06440a6e34dce8235ce589c4dd31de1ec9
Jhodel Cailan <jhodel.cailan@gmail.com>
2021-01-09 18:42:57 +0800

    Step 4: Event handling

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -3,11 +3,15 @@ import { Card, Text } from "@ui5/webcomponents-react";
 import { spacing } from "@ui5/webcomponents-react-base";
 
 export function MyApp() {
+	const handleHeaderClick = () => {
+		alert("Header clicked");
+	};
+
 	return (
 		<div>
-			<Card heading="Card" style={{ width: "300px" }}>
+			<Card heading="Card" style={{ width: "300px" }} headerInteractive onHeaderClick={handleHeaderClick}>
 				<Text style={spacing.sapUiContentPadding}>This is the content area of the Card</Text>
 			</Card>
 		</div>
 	);
-}
+};
