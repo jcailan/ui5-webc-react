e740e967fc555e2afcc8448b3107dabcedf422ab
Jhodel Cailan <jhodel.cailan@gmail.com>
2021-01-09 18:38:49 +0800

    Step 3: Style your component

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Card, Text } from "@ui5/webcomponents-react";
+import { spacing } from "@ui5/webcomponents-react-base";
 
 export function MyApp() {
 	return (
 		<div>
-			<Card heading="Card">
-				<Text>This is the content area of the Card</Text>
+			<Card heading="Card" style={{ width: "300px" }}>
+				<Text style={spacing.sapUiContentPadding}>This is the content area of the Card</Text>
 			</Card>
 		</div>
 	);
