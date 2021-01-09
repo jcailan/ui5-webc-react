aa14ea6bd86cfe6aba9c5fca364628a96b73cae9
Jhodel Cailan <jhodel.cailan@gmail.com>
2021-01-09 22:29:53 +0800

    Step 4: Import Router components

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Avatar, ShellBar, ShellBarItem } from "@ui5/webcomponents-react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { Home } from "./Home";
+import { Detail } from "./Detail";
 import "@ui5/webcomponents-icons/dist/add.js";
 
 export function MyApp() {
@@ -12,6 +15,12 @@ export function MyApp() {
 			>
 				<ShellBarItem icon="add" text="Add" />
 			</ShellBar>
+			<Switch>
+				<Route path="/home" component={Home} />
+				<Route path="/detail" component={Detail} />
+				<Redirect from="/" to="/home" />
+			</Switch>
+
 		</div>
 	);
 };
